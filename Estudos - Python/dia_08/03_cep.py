1e34c639518b42d2278f987f70205d40cb52f6aa
Carlos Eduardo <93060135+edu2406@users.noreply.github.com>
2025-12-24 11:38:21 -0300

    Add files via upload

---
+++ Estudos - Python/dia_08/03_cep.py
@@ -0,0 +1,12 @@
+import requests
+
+cep = input("Entre com um CEP: ")
+
+url = "https://viacep.com.br/ws/{cep}/json/"
+dados = dict()
+resposta = requests.get(url.format(cep=cep))
+if resposta.status_code == 200:
+    dados = resposta.json()
+
+for chave, valor in dados.items(): 
+    print(chave, "->", valor)
